756e1b6ea107d39738b2363595d95d3f6b6fc4b8
Tatitest <tatisecret13@gmail.com>
2024-09-24 13:39:07 +0300

    added mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,18 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress tests report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
     baseUrl: "https://www.saucedemo.com/",
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
     watchForFileChanges: false,
